8cb9de775f8fa43ff9368b37808760eda5081910
Aspen92 <asplundchristofer@gmail.com>
2023-01-11 07:30:51 +0000

    Create ProfileDetail view and update urls.py

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -5,11 +6,25 @@ from .models import Profile
 from .serializers import ProfileSerializer
 
 
-# ProfileList API View
+# ProfileList API View - List all profiles.
 
 
 class ProfileList(APIView):
     def get(self, request):
         profiles = Profile.objects.all()
         serializer = ProfileSerializer(profiles, many=True)
+        return Response(serializer.data)
+
+
+class ProfileDetail(APIView):
+    def get_object(self, pk):
+        try:
+            profile = Profile.objects.get(pk=pk)
+            return profile
+        except Profile.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile)
         return Response(serializer.data)
