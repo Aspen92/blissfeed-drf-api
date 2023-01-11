690fbebe4b121dc4f27711ffdee544d672f20201
Aspen92 <asplundchristofer@gmail.com>
2023-01-11 06:46:42 +0000

    Create ProfileSerialier and add it to views.py

--- profiles/views.py
+++ profiles/views.py
@@ -2,9 +2,14 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
+from .serializers import ProfileSerializer
+
+
+# ProfileList API View
 
 
 class ProfileList(APIView):
     def get(self, request):
         profiles = Profile.objects.all()
-        return Response(profiles)
+        serializer = ProfileSerializer(profiles, many=True)
+        return Response(serializer.data)
