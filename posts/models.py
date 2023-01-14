237ac467df2685b3fe307165fa4f65dd58920864
Aspen92 <asplundchristofer@gmail.com>
2023-01-14 17:47:15 +0000

    Created image filter in posts/models.py

--- posts/models.py
+++ posts/models.py
@@ -1,19 +1,30 @@
 from django.db import models
 from django.contrib.auth.models import User
- 
+
 
 class Post(models.Model):
     """
-    Post model, related to 'owner', i.e. a User instance.
-    Default image set so that we can always reference image.url.
+    Post model, related to 'owner'.
     """
+    image_filter_choices = [
+        ('funny', 'Funny'),
+        ('awesome', 'Awesome'),
+        ('inspiration', 'Inspiration'),
+        ('health', 'Health'),
+        ('beautiful', 'Beautiful'),
+        ('amazing', 'Amazing'),
+    ]
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
     image = models.ImageField(
-        upload_to='images/', default='../default_post_rgq6aq', blank=True)
+        upload_to='images/', default='../post_default_cut8cb', blank=True)
+
+    image_filter = models.CharField(
+        max_length=32, choices=image_filter_choices, default='normal'
+    )
 
     class Meta:
         ordering = ['-created_at']
