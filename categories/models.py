1ef8280e583f3d4edb94ee139dd3a9acafd05db0
Aspen92 <asplundchristofer@gmail.com>
2023-02-26 09:19:35 +0000

    Changed typos in models.py for categories

--- categories/models.py
+++ categories/models.py
@@ -7,7 +7,7 @@ class Category(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering = [-'created_at']
+        ordering = ['-created_at']
 
     def __str__(self):
         return f'Category: {self.category}'
