1e60f7f94854ac9cb3d854f77879e15a26806342
Aspen92 <asplundchristofer@gmail.com>
2023-02-26 08:49:24 +0000

    Add meta class to categories modell

--- categories/models.py
+++ categories/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    category = models.CharField(max_length=60)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = [-'created_at']
+
+    def __str__(self):
+        return f'Category: {self.category}'
