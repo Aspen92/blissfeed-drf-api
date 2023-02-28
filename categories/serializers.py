50d6709e65ed34d04de5f9ed00d2698a1c56beeb
Aspen92 <asplundchristofer@gmail.com>
2023-02-28 13:55:32 +0000

    Update categories serializer with new fields due to categories names not showing up

--- categories/serializers.py
+++ categories/serializers.py
@@ -1,20 +1,10 @@
-# from rest_framework import serializers
-# from categories.models import Category
-
-
-# class CategorySerializer(serializers.ModelSerializer):
-#     category = serializers.ReadOnlyField(source='category.category')
-
-#     class Meta:
-#         model = Category
-#         fields = ['id', 'category', 'created_at', 'updated_at']
-
 from rest_framework import serializers
 from categories.models import Category
+
 
 class CategorySerializer(serializers.ModelSerializer):
     name = serializers.CharField(source='category', read_only=True)
 
     class Meta:
         model = Category
-        fields = ['id', 'name', 'created_at', 'updated_at']
+        fields = ['name', 'created_at', 'updated_at']
